4ad7b0934af50d09c73140cf99c85a36218fe9e4
CastroMaster10 <rudacate02@hotmail.com>
2020-04-06 16:34:40 -0500

    Primer commit de platzivideo, con el curso de react.js

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../assets/styles/App.scss';  // hacer referencia al prepocesador SASS 
+
+const HelloWorld = () => (
+    <h1>Hola  Ruben este es un mensaje sereto, nos podemos juntar hoy a las 4?</h1>
+    
+);
+
+export default HelloWorld;
