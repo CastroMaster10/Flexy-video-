6f1b682c13e7f87ef6541d59e1c0b69514752a4e
CastroMaster10 <rudacate02@hotmail.com>
2020-04-06 18:20:32 -0500

    Correcion de declaracion de React al componente del Header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/Header'
+import Header from './components/Header';
+
 
 ReactDOM.render(<Header/>, document.getElementById("app"));
 
