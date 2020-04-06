314b42b5943b4300064dc287e86bc98a8aef4da8
CastroMaster10 <rudacate02@hotmail.com>
2020-04-06 17:57:23 -0500

    Se le agrego el componente del Header.

--- src/components/HelloWorld.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../assets/styles/App.scss';  // hacer referencia al prepocesador SASS 
-
-const HelloWorld = () => (
-    <h1>Hola  Ruben este es un mensaje sereto, nos podemos juntar hoy a las 4?</h1>
-    
-);
-
-export default HelloWorld;
